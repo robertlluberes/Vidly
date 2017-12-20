465c51daeb49879541c866d87f67952e9f848c3d
robertlluberes <robertlluberes@gmail.com>
2017-12-20 15:07:31 -0400

    Customer DTO

---
+++ Vidly/Dtos/CustomerDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Vidly.Models;
+
+namespace Vidly.Dtos
+{
+    public class CustomerDto
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        [Min18YearIfAMember]
+        public DateTime? Birthdate { get; set; }
+
+        public bool IsSuscribedToNewsletter { get; set; }
+
+        public byte MembershipTypeId { get; set; }
+    }
+}
