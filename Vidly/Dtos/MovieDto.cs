990526ce235b1ba3597cf4395336f1d8a9be3116
robertlluberes <robertlluberes@gmail.com>
2017-12-21 13:26:04 -0400

    Created MoviesControler API and the MovieDto. Fixed Error casting between model class and DTOs

---
+++ Vidly/Dtos/MovieDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Vidly.Dtos
+{
+    public class MovieDto
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+
+        public DateTime DateAdded { get; set; }
+
+        public byte NumberInStock { get; set; }
+
+        public byte GenreId { get; set; }
+    }
+}
