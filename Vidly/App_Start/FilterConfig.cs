c2a8ba2c878247bfb53f864e494be8b0678cb001
robertlluberes <robertlluberes@gmail.com>
2017-12-26 09:24:52 -0400

    Revert "Adding Global AuthorizeAttribute filter"
    This reverts commit 5371cdfe6238abefbf59595528aa39855188f453.

--- Vidly/App_Start/FilterConfig.cs
+++ Vidly/App_Start/FilterConfig.cs
@@ -1,4 +1,5 @@
-﻿using System.Web.Mvc;
+﻿using System.Web;
+using System.Web.Mvc;
 
 namespace Vidly
 {
@@ -7,7 +8,6 @@ namespace Vidly
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
-            filters.Add(new AuthorizeAttribute());
         }
     }
 }
