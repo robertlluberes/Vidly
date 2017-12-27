57661f45762acae883374f52cd4184a2c6e0daa8
robertlluberes <robertlluberes@gmail.com>
2017-12-27 10:21:15 -0400

    Added NewRentals Controller with CreateNewRental action not implemented yet. Added DTO NewRentalDto to recibe the data from client

---
+++ Vidly/Controllers/API/NewRentalsController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Web.Http;
+
+namespace Vidly.Controllers.API
+{
+    public class NewRentalsController : ApiController
+    {
+        [HttpPost]
+        public IHttpActionResult CreateNewRental()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
