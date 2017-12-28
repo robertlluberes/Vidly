64d82a6ba1463b4036e7fa38fc9928f0c544629e
robertlluberes <robertlluberes@gmail.com>
2017-12-28 11:03:52 -0400

    Improving filtering movies and customer in the API controllers. Improving look and feel

--- Vidly/Controllers/API/MoviesController.cs
+++ Vidly/Controllers/API/MoviesController.cs
@@ -24,9 +24,17 @@ namespace Vidly.Controllers.API
         }
 
         // /api/customers
-        public IHttpActionResult GetMovies()
+        public IHttpActionResult GetMovies(string query = null)
         {
-            var movies = _context.Movies.Include(g => g.Genre).ToList().Select(Mapper.Map<Movie, MovieDto>);
+            var movies = _context.Movies
+                                 .Include(g => g.Genre)
+                                 .Where(m => m.NumberAvailable > 0);
+
+            if (!string.IsNullOrWhiteSpace(query))
+                movies = movies.Where(m => m.Name.Contains(query));
+
+            movies.ToList().Select(Mapper.Map<Movie, MovieDto>);
+
             return Ok(movies);
         }
 
